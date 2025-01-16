4a4f412fb2d29a961316f84b00c81b018e73c7dd
AndileThabethe <151631281+AndileThabethe@users.noreply.github.com>
2025-01-16 10:58:53 +0200

    feat: inject reservation service into form component

--- hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 
 @Component({
   selector: 'app-reservation-form',
@@ -9,7 +11,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ReservationFormComponent implements OnInit{
   reservationForm: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder){
+  constructor(
+    private formBuilder: FormBuilder, 
+    private reservationService: ReservationService){
 
   }
 
@@ -25,7 +29,9 @@ export class ReservationFormComponent implements OnInit{
 
   onSubmit() {
     if(this.reservationForm.valid){
-      console.log()
+
+      let reservation: Reservation = this.reservationForm.value;
+      this.reservationService.addReservation(reservation)
     }
   }
 }
